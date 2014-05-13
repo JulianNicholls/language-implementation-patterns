17d09e3e91076f128eaaab3d8d210825f8c89a51
Julian Nicholls <juliannicholls29@gmail.com>
2014-05-13 17:54:46 +0100

    Added explanation of test.

--- test_4.rb
+++ test_4.rb
@@ -2,6 +2,11 @@
 
 require 'llklistparser'
 
+# Parse slightly more complicated lists:
+#   [a, b, c]
+#   [a, [b, c], d]
+#   [a=b, c = d, [d, f], q]
+
 lexer   = ListLexer.new( ARGV[0] )
 parser  = ListParser.new( lexer, 2 )  # Only needs to look ahead one
 parser.list
